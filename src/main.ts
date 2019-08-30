3c8ca4e3449d8cc28e1a127635827d69a036fe21
Oleg <oleg.chorny@gmail.com>
2019-08-30 11:17:38 +0300

    first exec

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import * as core from '@actions/core';
+//import * as exec from '@actions/exec';
 
+const exec = require('@actions/exec');
+
+
+async function pip() {
+  await exec.exec('pip install wheel');
+  await exec.exec('pip install python-openstackclient');
+  await exec.exec('pip install python-magnumclient');
+}
+
+run();
 async function run() {
   try {
     const myInput = core.getInput('myInput');
